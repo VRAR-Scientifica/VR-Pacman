2c3380b45263cc6aabd01cacdcaaf85158e05390
VRAR-Scientifica <compvrar@gmail.com>
2018-10-05 14:42:01 +0530

    movement for pacman

---
+++ Assets/Movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    public Transform vrCamera;
+    public float speed;
+    public float toggleangle;
+    private CharacterController cc;
+    private bool moveforward;
+	// Use this for initialization
+	void Start () {
+        cc = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if((vrCamera.transform.eulerAngles.x <= toggleangle && vrCamera.transform.eulerAngles.x >= 0f) || (vrCamera.transform.eulerAngles.x >= 360f-toggleangle && vrCamera.transform.eulerAngles.x <= 360f))
+        {
+            moveforward = true;
+        }
+        else
+        {
+            moveforward = false;
+        }
+        
+        if(moveforward)
+        {
+            Vector3 forward = vrCamera.TransformDirection(Vector3.forward);
+            cc.SimpleMove(forward * speed);
+        }
+	}
+}
